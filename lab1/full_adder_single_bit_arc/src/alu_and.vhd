7e8a22af0cf0f8592d7d688e1e67a023cbf905f3
Matthew Allen <AechPro@users.noreply.github.com>
2016-10-20 10:25:43 -0400

    Create alu_and.vhd

---
+++ lab1/full_adder_single_bit_arc/src/alu_and.vhd
@@ -0,0 +1,22 @@
+---------------------------------
+--       Matthew Alllen        --
+--         09/1/2016           --
+--           Lab 1             --
+--  Basic And gate component   --
+---------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity alu_and is port
+(
+  a : in std_logic;
+  b : in std_logic;
+  output : out std_logic
+);
+end alu_and;
+
+architecture arc of alu_and is
+begin
+  output <= a and b;
+end arc;
