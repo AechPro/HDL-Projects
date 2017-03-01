06927fd59329c3684f96af5f391b4a19dab8c4ad
Matthew Allen <AechPro@users.noreply.github.com>
2017-03-01 08:28:29 -0500

    Add files via upload

---
+++ lab3/seven_segment_counter/sim/script/simulate.bat
@@ -0,0 +1,9 @@
+vsim -do sim.do
+del /F /Q work
+rmdir -force work
+del /F transcript
+del /F vsim.wlf
+del /F vsim_stacktrace.vstf
+pushd S:\"HDL labs"
+svn add . --force
+svn commit -m "Automatic script commit"
